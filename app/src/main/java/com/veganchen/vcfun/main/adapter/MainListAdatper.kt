4479501cc039f7b89e31f5fa1688ada52f51378d
g-vc <veganchen@yeah.net>
2020-04-19 15:15:55 +0800

    some code
    1.some code

---
+++ app/src/main/java/com/veganchen/vcfun/main/adapter/MainListAdatper.kt
@@ -0,0 +1,29 @@
+package com.veganchen.vcfun.main.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * 主界面列表adapter
+ */
+class MainListAdatper : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    class ItemHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private lateinit var tvItem : TextView
+        
+    }
+}
